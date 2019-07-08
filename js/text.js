71bde807ec45bd0265d5ff57a5f5126e69468c9d
TheDragonFire961 <37818652+TheDragonFire961@users.noreply.github.com>
2019-07-08 19:40:21 +1000

    Update text.js
    Fix whitespace

--- js/text.js
+++ js/text.js
@@ -1,5 +1,5 @@
 /*
-    textjs: contains JS for working with text.
+    text.js: contains JS for working with text.
     
     Copyright (C) 2019 Ethan/TheDragonFire961.
     This file is part of Education Cybersafety.
@@ -27,79 +27,78 @@ class textClass {
 	// Following are HTML tag construct functions.
 	// textClass.NewP(): creates new paragraph. It exposes HTML attributes, with specific fields for class and id.
 	NewP(content = '', HTMLclass = '', id = '', others = '') {
-		var parameters = '';
-		if (HTMLclass !== '') {
-		    parameters += ' class="' + HTMLclass + '"';
-		} 
-		if (id !== '') {
-		    parameters += ' id="' + id + '"';
-		} 
-		if (others !== '') {
-		    parameters += ' ' + others;
+        var parameters = '';
+        if (HTMLclass !== '') {
+            parameters += ' class="' + HTMLclass + '"';
 		}
-	    return "<p" + parameters + ">" + content + "</p>";
-	}
-	// textClass.NewA(): creates a new hyperlink (a tag).
-	NewA(label = '', href = 'button', onclickTarget = '', rel = 'noopener', target = '_self', id = '', HTMLclass = '', others = '') {
-		var parameters = '';
-		parameters += ' type="' + type + '"'; 
-		if (onclickTarget !== '') {
-		    parameters += ' onclick="' + onclickTarget + '"';
+        if (id !== '') {
+            parameters += ' id="' + id + '"';
+        }
+        if (others !== '') {
+            parameters += ' ' + others;
+		}
+        return "<p" + parameters + ">" + content + "</p>";
+    }
+    // textClass.NewA(): creates a new hyperlink (a tag).
+    NewA(label = '', href = 'button', onclickTarget = '', rel = 'noopener', target = '_self', id = '', HTMLclass = '', others = '') {
+        var parameters = '';
+        parameters += ' type="' + type + '"'; 
+        if (onclickTarget !== '') {
+            parameters += ' onclick="' + onclickTarget + '"';
 		}
         if (href !== '') {
-		    parameters += ' href="' + href + '"';
-		} 
+            parameters += ' href="' + href + '"';
+        }
         // There should better be a good reason why rel and target are empty...
-		parameters += ' rel="' + rel + '"';
-		parameters += ' target="' + target + '"';
-		if (id !== '') {
-		    parameters += ' id="' + id + '"';
-		} 
-		if (HTMLclass !== '') {
-		    parameters += ' class="' + HTMLclass + '"';
-		}
-	    return "<button" + parameters + ">" + label + "</button>";
-	}
-	// textClass.NewButton() creates a new button.
-	NewButton(label = '', type = 'button', onclickTarget = '', id = '', HTMLclass = '', others = '') {
-		var parameters = '';
-		parameters += ' type="' + type + '"'; 
-		if (onclickTarget !== '') {
-		    parameters += ' onclick="' + onclickTarget + '"';
-		} 
-		if (id !== '') {
-		    parameters += ' id="' + id + '"';
-		} 
-		if (HTMLclass !== '') {
-		    parameters += ' class="' + HTMLclass + '"';
-		}
-	    return "<button" + parameters + ">" + label + "</button>";
-	}
-	// textClass.NewTag(): creates any body tag.
-	NewTag(content = '') {
-		this.content.innerHTML += content; // Note you need to know what tag you want to put in your head; the function won't do that for you!
-	    return;
-	}
-	// textClass.NewImg(); creates a new HTML image. Meta tag for textClass.NewImage(); yes, there is a difference between the two.
-	// Do not use textClass.NewImg() unless the image you are referring to is either public domain, CC0-licensed or similarly public domain somehow.
-	NewImg(path = '', alt = '', HTMLclass = '', id = '') {
-		// NO hotlinked images, since the QPCC overlords _will_ kick your ass if we do.
+        parameters += ' rel="' + rel + '"';
+        parameters += ' target="' + target + '"';
+        if (id !== '') {    
+            parameters += ' id="' + id + '"';
+        } 
+        if (HTMLclass !== '') {
+            parameters += ' class="' + HTMLclass + '"';
+        }
+        return "<button" + parameters + ">" + label + "</button>";
+    }
+    // textClass.NewButton() creates a new button.
+    NewButton(label = '', type = 'button', onclickTarget = '', id = '', HTMLclass = '', others = '') {
+        var parameters = '';
+        parameters += ' type="' + type + '"'; 
+        if (onclickTarget !== '') {
+            parameters += ' onclick="' + onclickTarget + '"';
+        } 
+        if (id !== '') {
+            parameters += ' id="' + id + '"';
+        } 
+        if (HTMLclass !== '') {
+            parameters += ' class="' + HTMLclass + '"';
+        }
+        return "<button" + parameters + ">" + label + "</button>";
+    }
+    // textClass.NewTag(): creates any body tag.
+    NewTag(content = '') {
+        this.content.innerHTML += content; // Note you need to know what tag you want to put in your head; the function won't do that for you!
+        return;
+    }
+    // textClass.NewImg(); creates a new HTML image. Meta tag for textClass.NewImage(); yes, there is a difference between the two.
+    // Do not use textClass.NewImg() unless the image you are referring to is either public domain, CC0-licensed or similarly public domain somehow.
+    NewImg(path = '', alt = '', HTMLclass = '', id = '') {
+        // NO hotlinked images, since the QPCC overlords _will_ kick your ass if we do.
         // The W3C overlords say that alt text is _mandatory_ on all <img>s, so don't bother checking that crap to see if it's empty.
-		var parameters;
-		// Throw warning if the path looks like a URL (not allowed);
-		if (path.match(/^(https?|ftp|torrent|image|irc):\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?$/i)) {
-			console.warn("Hotlinking of image(s) detected. You cannot hotlink images; please download locally and import.");
+        var parameters;
+        // Throw warning if the path looks like a URL (not allowed);
+        if (path.match(/^(https?|ftp|torrent|image|irc):\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?$/i)) {
+            console.warn("Hotlinking of image(s) detected. You cannot hotlink images; please download locally and import.");
 		}
-		
-	}
+    }
     NewImage(path = '', alt = '', imgRef = '', onclickTarget = '', HTMLclass = '', others= '', id = '', imgHTMLClass = '', imgId = '', imgOthers = '') {
 		// We create the image by wrapping it with the image's image ID (assigned in js/imageRegistry.js).
-		// It is borne out of two parts: an <a> tag to provide a hyperlink and an <img> tag inside.
+        // It is borne out of two parts: an <a> tag to provide a hyperlink and an <img> tag inside.
         //
-		// Best create the URL before injecting it
-		var imgRefFull = 'index.html?mode=image&amp;imgRef=' + imgRef;
-		// Return a recursively created hyperlink and image combo.
-		return this.NewA(this.NewImg(path, alt, imgHTMLClass, imgId, imgOthers), imgRef, onclickTarget, 'noopener', '_blank', id, HTMLClass, others);
+        // Best create the URL before injecting it
+        var imgRefFull = 'image_desc.html?mode=image&amp;imgRef=' + imgRef; // Remember, it's &amp; NOT &
+        // Return a recursively created hyperlink and image combo.
+        return this.NewA(this.NewImg(path, alt, imgHTMLClass, imgId, imgOthers), imgRef, onclickTarget, 'noopener', '_blank', id, HTMLClass, others);
 	}
 	// Following are publishing functions. The constructor functions _don't_ publish text 
 	// automatically; it must be passed through a publishing function.
@@ -109,10 +108,10 @@ class textClass {
 	}
 	// textClass.Clear(): Clears the DOM entirely. Useful for resetting the page?
 	Clear() {
-		return this.content.innerHTML = '';
-	}
-	// textClass.Queue(): holds the text in a queue, prints it and/or deletes it. To queue, pass 
-	// textClass.Queue(content). Replace it with textClass.Queue(true) to publish the contents of the queue.
+        return this.content.innerHTML = '';
+    }
+    // textClass.Queue(): holds the text in a queue, prints it and/or deletes it. To queue, pass 
+    // textClass.Queue(content). Replace it with textClass.Queue(true) to publish the contents of the queue.
     // To flush it, pass (textClass.Queue('anything', true);.
 	Queue(content, flush) {
 		if (content === true) {
@@ -140,6 +139,7 @@ class textClass {
 // This class contains constructs for a single in-text reference. We use the WP format here: they
 // look like this: [1] and can be hovered over by the user to get a reference.
 class Ref {
+    // I don't know whether I'll finish this on time TBH
 	constructor() {
 		
 	}
